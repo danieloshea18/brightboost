40f34caf9d638e375df4d5c2f7beb505b32ffa87
danieloshea18 <danieloshea856@gmail.com>
2025-06-11 16:05:48 -0400

    StudentDashboard

---
+++ brightboost-student-roster/app/page.tsx
@@ -0,0 +1,9 @@
+import StudentRoster from "@/components/student-roster"
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-10 px-4">
+      <StudentRoster />
+    </main>
+  )
+}
