f94277c2117ef680ab3ebca33ef7fb1d8d9c2aec
danieloshea18 <danieloshea856@gmail.com>
2025-06-16 22:13:28 -0400

    Delete brightboost-student-roster directory

--- brightboost-student-roster/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import StudentRoster from "@/components/student-roster"
-
-export default function Home() {
-  return (
-    <main className="container mx-auto py-10 px-4">
-      <StudentRoster />
-    </main>
-  )
-}
