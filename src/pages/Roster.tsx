1847d3b37ae784a5e823292c12095c86adc3bb63
danieloshea18 <danieloshea856@gmail.com>
2025-06-28 14:15:59 -0400

    Update Roster.tsx

--- src/pages/Roster.tsx
+++ src/pages/Roster.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import StudentRoster from "../student-roster"
+import StudentRoster from "../components/TeacherDashboard/Student-Roster"
 
 export default function Page() {
   return <StudentRoster />
